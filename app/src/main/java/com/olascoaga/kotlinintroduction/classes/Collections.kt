96a27b8f623e0d82054712f790b0ae62b480d3f2
Uriel <uriel1396@gmail.com>
2023-08-15 09:40:15 -0600

    Added mutable list example

--- app/src/main/java/com/olascoaga/kotlinintroduction/classes/Collections.kt
+++ app/src/main/java/com/olascoaga/kotlinintroduction/classes/Collections.kt
@@ -7,4 +7,34 @@ fun main() {
     val fruits = listOf("Apple", "Banana", "Grape", "Lima")
     println(fruits[(fruits.indices).random()])
     println("Grape index is: ${fruits.indexOf("Grape")}")
+
+    createNewTopic("Collections (Mutable)")
+
+    val user = User(0, "Drake", "Parker", Group.FAMILY.ordinal)
+    val brother = user.copy(1, "Josh", "Nichols")
+    val friend = brother.copy(id = 2, group = Group.FRIENDS.ordinal)
+    val users = mutableListOf<User>(user, brother)
+    println(users)
+
+    users.add(friend)
+    println(users)
+
+    users.removeAt(1)
+    println(users)
+
+    users.remove(user)
+    println(users)
+
+    val usersSelectedList = mutableListOf<User>()
+    println(usersSelectedList)
+
+    usersSelectedList.add(user)
+    println(usersSelectedList)
+
+    usersSelectedList[0] = brother
+    println(usersSelectedList)
+
+    usersSelectedList.add(user)
+    usersSelectedList.add(user)
+    println(usersSelectedList)
 }
